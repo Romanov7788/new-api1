336ed7c8fa40557d56ea461a98dcf1b969ad56ce
Romanov7788 <romanov_sash@ukr.net>
2022-05-26 21:45:08 +0300

    finish

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -2,7 +2,7 @@ const userService = require("../service/user-service");
 const { validationResult } = require("express-validator");
 const ApiError = require("../exceptions/api-error");
 const UserModel = require("../models/user-model");
-const FriendShipService = require('../service/friend-service');
+const FriendShipService = require("../service/friend-service");
 
 class UserController {
   async registration(req, res, next) {
@@ -59,42 +59,49 @@ class UserController {
   async addFriends(req, res, next) {
     try {
       const { friendsId, myId } = req.body;
-      const userData = await FriendShipService.addFriends(friendsId, myId);
+      await FriendShipService.addFriends(friendsId, myId);
       return res.json({
         status: "success",
         message: "Friend request has been sent.",
       });
     } catch (e) {
-      next(e);
+      res.json({
+        status: "error",
+        message: `Sorry we have some problem with ${e}`,
+      });
     }
   }
 
-  async rejectedFriends(req, res, next) {
+  rejectedFriends(req, res, next) {
     try {
       const { friendsId, myId } = req.body;
-      const userData = await FriendShipService.rejectedFriends(
-        friendsId,
-        myId
-      );
+      FriendShipService.rejectedFriends(friendsId, myId);
       return res.json({
         status: "success",
         message: "Friend rejected.",
       });
-    } catch (e) {}
+    } catch (e) {
+      res.json({
+        status: "error",
+        message: `Sorry we have some problem with ${e}`,
+      });
+    }
   }
 
   async acceptFriends(req, res, next) {
     try {
       const { friendsId, myId } = req.body;
-      const userData = await FriendShipService.acceptFriends(
-        friendsId,
-        myId
-      );
+      await FriendShipService.acceptFriends(friendsId, myId);
       return res.json({
         status: "success",
         message: "Friend accepted.",
       });
-    } catch (e) {}
+    } catch (e) {
+       res.json({
+        status: "error",
+        message: `Sorry we have some problem with ${e}`,
+      });
+    }
   }
 }
 
--- service/friend-service.js
+++ service/friend-service.js
@@ -2,10 +2,8 @@ const UserModel = require("../models/user-model");
 
 class FriendShipService {
   async addFriends(friendsId, myId) {
-    UserModel.updateOne(
-      {
-        _id: friendsId,
-      },
+    await UserModel.updateOne(
+      { _id: friendsId },
       {
         $push: {
           incomingRequests: {
@@ -13,31 +11,24 @@ class FriendShipService {
             status: "Pending",
           },
         },
-      },
-      function () {
-        UserModel.updateOne(
-          {
-            _id: myId,
+      }
+    );
+    await UserModel.updateOne(
+      { _id: myId },
+      {
+        $push: {
+          outcomingRequests: {
+            _id: friendsId,
+            status: "Pending",
           },
-          {
-            $push: {
-              outcomingRequests: {
-                _id: friendsId,
-                status: "Pending",
-              },
-            },
-          },
-          function () {} // не понимаю, без этой пустой функции не записывает в outcomingRequests результат
-        );
+        },
       }
     );
   }
 
   async rejectedFriends(friendsId, myId) {
-    UserModel.updateMany(
-      {
-        _id: friendsId,
-      },
+    await UserModel.updateMany(
+      { _id: friendsId },
       {
         $unset: {
           incomingRequests: {
@@ -45,61 +36,52 @@ class FriendShipService {
             status: "Rejected",
           },
         },
+      }
+    );
+    await UserModel.updateMany(
+      {
+        _id: myId,
       },
-      function () {
-        UserModel.updateMany(
-          {
+      {
+        $unset: {
+          outcomingRequests: {
+            _id: friendsId,
+            status: "Rejected",
+          },
+        },
+      }
+    );
+    await UserModel.updateOne(
+      {
+        _id: friendsId,
+      },
+      {
+        $push: {
+          rejectedRequests: {
             _id: myId,
+            status: "Rejected",
           },
-          {
-            $unset: {
-              outcomingRequests: {
-                _id: friendsId,
-                status: "Rejected",
-              },
-            },
+        },
+      }
+    );
+    await UserModel.updateOne(
+      {
+        _id: myId,
+      },
+      {
+        $push: {
+          rejectedRequests: {
+            _id: friendsId,
+            status: "Rejected",
           },
-          function () {
-            UserModel.updateOne(
-              {
-                _id: friendsId,
-              },
-              {
-                $push: {
-                  rejectedRequests: {
-                    _id: myId,
-                    status: "Rejected",
-                  },
-                },
-              },
-              function () {
-                UserModel.updateOne(
-                  {
-                    _id: myId,
-                  },
-                  {
-                    $push: {
-                      rejectedRequests: {
-                        _id: friendsId,
-                        status: "Rejected",
-                      },
-                    },
-                  },
-                  function () {} // не понимаю, без этой пустой функции не записывает в outcomingRequests результат
-                );
-              }
-            );
-          }
-        );
+        },
       }
     );
   }
 
   async acceptFriends(friendsId, myId) {
-    UserModel.updateMany(
-      {
-        _id: friendsId,
-      },
+    await UserModel.updateMany(
+      { _id: friendsId },
       {
         $unset: {
           incomingRequests: {
@@ -107,50 +89,37 @@ class FriendShipService {
             status: "Pending",
           },
         },
-      },
-      function () {
-        UserModel.updateMany(
-          {
+      }
+    );
+    await UserModel.updateMany(
+      { _id: myId },
+      {
+        $unset: {
+          outcomingRequests: {
+            _id: friendsId,
+            status: "Pending",
+          },
+        },
+      }
+    );
+    await UserModel.updateOne(
+      { _id: friendsId },
+      {
+        $push: {
+          friends: {
             _id: myId,
           },
-          {
-            $unset: {
-              outcomingRequests: {
-                _id: friendsId,
-                status: "Pending",
-              },
-            },
+        },
+      }
+    );
+    await UserModel.updateOne(
+      { _id: myId },
+      {
+        $push: {
+          friends: {
+            _id: friendsId,
           },
-          function () {
-            UserModel.updateOne(
-              {
-                _id: friendsId,
-              },
-              {
-                $push: {
-                  friends: {
-                    _id: myId,
-                  },
-                },
-              },
-              function () {
-                UserModel.updateOne(
-                  {
-                    _id: myId,
-                  },
-                  {
-                    $push: {
-                      friends: {
-                        _id: friendsId,
-                      },
-                    },
-                  },
-                  function () {} // не понимаю, без этой пустой функции не записывает в outcomingRequests результат
-                );
-              }
-            );
-          }
-        );
+        },
       }
     );
   }
