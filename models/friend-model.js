b45ac28efd2e7a85e4372622b38f6d3bf49a7bf3
Romanov7788 <romanov_sash@ukr.net>
2022-05-27 14:25:17 +0300

    i

--- models/friend-model.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const addFriendsSchema = new Schema({
-  id: { type: String },
-  _ids: { type: Array },
-});
-
-module.exports = model("addFriends", addFriendSchema);
